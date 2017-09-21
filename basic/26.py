f32e20b5d026fa5af4326cb6afcd9eba37b7fa1e
Fucheng Wong <wyn@FuchengdeMBP.lan>
2017-09-21 09:35:47 +0800

    26

--- basic/26.py
+++ basic/26.py
@@ -1,3 +1,5 @@
+# _*_ coding: utf-8 _*_
+
 """
 26. Write a Python program to create a histogram from a given list of integers.
 
@@ -12,3 +14,25 @@
 6 3 8 2 5 0 4 
 
 """
+
+
+
+def print_histogram(data):
+    top = max(data)
+
+    while top > 0:
+        for n in data:
+            if n >= top:
+                print '█',
+            else:
+                print ' ',
+        print ''
+        top -= 1
+
+    for n in data:
+        print n,
+
+
+lst = [6, 3, 8, 2, 5, 0, 4]
+print_histogram(lst)
+
